d60bb7323093e7c3d192fecabdc6e886ce7ad7d4
Keoviengkhone <79627905+Keoviengkhone@users.noreply.github.com>
2024-09-19 11:28:16 +0700

    show loading indecator

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -1,3 +1,12 @@
 import 'package:get/get.dart';
 
-class ProfileController extends GetxController {}
+class ProfileController extends GetxController {
+  var loading = false.obs;
+  void loadDataFromAPI() async {
+    loading.value = true;
+    await Future.delayed(
+      Duration(seconds: 5),
+    );
+    loading.value = false;
+  }
+}
